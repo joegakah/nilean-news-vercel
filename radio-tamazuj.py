a164f4568528a85d6140d7e3325f47b50c3e9683
JosephGakah <joegakah@gmail.com>
2025-02-27 01:06:05 +0300

    Enhance web scraping functionality to retrieve and save news articles from Radio Tamazuj in JSON format

--- radio-tamazuj.py
+++ radio-tamazuj.py
@@ -1,18 +1,70 @@
+import json
 import requests
 from bs4 import BeautifulSoup
+from markdownify import MarkdownConverter
 
 url = 'https://www.radiotamazuj.org/en/news'
 
-response = requests.get(url)
+def get_article_data(article_url):
+    
+    response = requests.get(article_url)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        title = soup.find('h1', class_='entry-title').get_text(strip=True)
+        date = soup.find('span', class_='posts-date').get_text(strip=True)
+        category = soup.find('a', rel='category tag').get_text(strip=True)
+        content = soup.find('div', class_='entry-content')
+        converter = MarkdownConverter()
+        content_md = converter.convert_soup(content)
+        
+        return {
+            'title': title,
+            'content': content_md,
+            'date': date,
+            'category': category
+        }
+    
+    else:
+        return "Error: Unable to retrieve article data"
 
-    articles = soup.find_all('div', class_='spotlight-post-1')
+def get_articles(url):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        articles = soup.find_all('div', class_='spotlight-post-1')
+        
+        for article in articles:
+            article_url = article.find('a', class_='em-figure-link')['href']
+            title = article.find('h3', class_='article-title-2').get_text(strip=True)
+            image = article.find('img', class_='wp-post-image')['src']
+            date = article.find('span', class_='posts-date').get_text(strip=True)
 
-    for article in articles:
-        title = article.find('h3', class_='article-title-2').get_text()
-        link = article.find('a', class_='em-figure-link')['href'] 
-        print(f'Title: {title}\nLink: {link}\n')
-else:
-    print("Failed to retrieve the webpage.")
+            article_data = get_article_data(article_url)
+
+            the_article = {
+                'title': title,
+                'author': '',
+                'url': article_url,
+                'imageUrl': image,
+                'description': '',
+                'content': article_data['content'],
+                'publishedAt': date,
+                'category': article_data['category'],
+                'source': 'radiotamazuj.org'
+            }
+
+            json_article = json.dumps(the_article, indent=4)
+            
+            with open(f'{title}.json', 'w') as f:
+                f.write(json_article)
+
+            print(json_article)
+        
+    else:
+        return "Error: Unable to retrieve article links"
+    
+get_articles(url)
